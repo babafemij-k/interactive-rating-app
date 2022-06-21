dfbc745438dab949c58d361ce73e6c919cae3677
babafemij-k <olaolujkb21@gmail.com>
2022-06-21 14:10:50 +0100

    Added hover state to rating buttons

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -2,7 +2,10 @@ import React from "react";
 
 const Rating = ({ rating, setFinalRating }) => {
   return (
-    <button onClick={() => setFinalRating(rating)} className="back-circle">
+    <button
+      onClick={() => setFinalRating(rating)}
+      className="back-circle selected"
+    >
       <h4>{rating}</h4>
     </button>
   );
